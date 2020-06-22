5cacfa0de671b8f369330cfb3275591cad57ad44
theclassical-dev <theclassical001@gmail.com>
2020-06-22 06:18:40 -0700

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-nav tabs>
+        <b-nav-item disabled><b>Quiz App</b></b-nav-item>
+        <b-nav-item disabled><b>Total Quiz Question: 10</b></b-nav-item>
+        <b-nav-item disabled>Score: {{ numCorrect}}</b-nav-item>
+        <b-nav-item disabled>Q/Answered: {{ numTotal }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'numCorrect',
+    'numTotal'
+  ]
+}
+</script>
